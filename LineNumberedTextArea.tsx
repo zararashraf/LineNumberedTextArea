8af0b79c731b743bcab7e99e08654f7e4e128b78
Zarar Ashraf <36181292+zararashraf@users.noreply.github.com>
2023-09-01 12:42:39 +0500

    Update LineNumberedTextArea.tsx
    Fixed stuff

--- LineNumberedTextArea.tsx
+++ LineNumberedTextArea.tsx
@@ -2,7 +2,6 @@ import React, { ChangeEvent, useState } from 'react';
 
 const LineNumberedTextArea: React.FC = () => {
   const [text, setText] = useState<string>('');
-  const maxVisibleRows = 25;
 
   const handleTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value);
@@ -10,10 +9,20 @@ const LineNumberedTextArea: React.FC = () => {
 
   const generateLineNumbers = () => {
     const totalRows = text.split('\n').length;
-    
+
     const lineNumbers: number[] = [];
 
-    for (let i = 1; i <= totalRows; i++) {
+    for (let i = 26; i <= totalRows; i++) {
+      lineNumbers.push(i);
+    }
+
+    return lineNumbers;
+  };
+
+  const hardcodedLineNumbers = () => {
+    const lineNumbers: number[] = [];
+
+    for (let i = 1; i <= 25; i++) {
       lineNumbers.push(i);
     }
 
@@ -21,18 +30,22 @@ const LineNumberedTextArea: React.FC = () => {
   };
 
   return (
-    <div className="flex">
-      <div className="w-16 h-96 overflow-y-auto bg-gray-200 text-right border-r border-gray-300">
+    <div className="flex text-sm">
+      <div className="w-16 h-96 overflow-y-hidden bg-gray-200 text-right border-r border-gray-300 text-gray-500">
+        {hardcodedLineNumbers().map((lineNumber) => (
+          <div key={lineNumber} className="pr-2 font-mono">
+            {lineNumber}
+          </div>
+        ))}
         {generateLineNumbers().map((lineNumber) => (
-          <div key={lineNumber} className="py-1 pl-2 pr-4 font-mono">
+          <div key={lineNumber} className="pr-2 font-mono">
             {lineNumber}
           </div>
         ))}
       </div>
       <div className="flex-1">
         <textarea
-          className="resize-none p-2 w-full h-96 border-l border-gray-300 outline-none overflow-y-auto font-mono"
-          rows={maxVisibleRows}
+          className="resize-none mt-0 pt-0 p-2 w-full h-96 border-y-2 border-r-2 rounded-r border-gray-300 outline-none overflow-y-auto font-mono"
           value={text}
           onChange={handleTextAreaChange}
         />
