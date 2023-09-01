cfe3f627393fd3cebb168831ac8c0ec6254ceefe
Zarar Ashraf <36181292+zararashraf@users.noreply.github.com>
2023-09-01 13:10:39 +0500

    Update LineNumberedTextArea.tsx
    Better

--- LineNumberedTextArea.tsx
+++ LineNumberedTextArea.tsx
@@ -1,43 +1,41 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 const LineNumberedTextArea: React.FC = () => {
   const [text, setText] = useState<string>('');
+  const [visibleLines, setVisibleLines] = useState<number[]>([]);
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
   const handleTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value);
   };
 
-  const generateLineNumbers = () => {
-    const totalRows = text.split('\n').length;
+  const calculateVisibleLines = () => {
+    if (textareaRef.current) {
+      const computedStyle = getComputedStyle(textareaRef.current);
+      const lineHeight = parseFloat(computedStyle.lineHeight);
+      const { scrollTop, clientHeight } = textareaRef.current;
+      const totalLines = text.split('\n').length;
+      const visibleCount = Math.floor(clientHeight / lineHeight);
+      const firstVisibleLine = Math.max(1, Math.ceil(scrollTop / lineHeight) + 1);
+      const lastVisibleLine = Math.min(totalLines, firstVisibleLine + visibleCount - 1);
+      const visibleLinesArray: number[] = [];
 
-    const lineNumbers: number[] = [];
+      for (let i = firstVisibleLine; i <= lastVisibleLine; i++) {
+        visibleLinesArray.push(i);
+      }
 
-    for (let i = 26; i <= totalRows; i++) {
-      lineNumbers.push(i);
+      setVisibleLines(visibleLinesArray);
     }
-
-    return lineNumbers;
   };
 
-  const hardcodedLineNumbers = () => {
-    const lineNumbers: number[] = [];
-
-    for (let i = 1; i <= 25; i++) {
-      lineNumbers.push(i);
-    }
-
-    return lineNumbers;
-  };
+  useEffect(() => {
+    calculateVisibleLines();
+  }, [text]);
 
   return (
     <div className="flex text-sm">
       <div className="w-16 h-96 overflow-y-hidden bg-gray-200 text-right border-r border-gray-300 text-gray-500">
-        {hardcodedLineNumbers().map((lineNumber) => (
-          <div key={lineNumber} className="pr-2 font-mono">
-            {lineNumber}
-          </div>
-        ))}
-        {generateLineNumbers().map((lineNumber) => (
+        {visibleLines.map((lineNumber) => (
           <div key={lineNumber} className="pr-2 font-mono">
             {lineNumber}
           </div>
@@ -45,9 +43,11 @@ const LineNumberedTextArea: React.FC = () => {
       </div>
       <div className="flex-1">
         <textarea
+          ref={textareaRef}
           className="resize-none mt-0 pt-0 p-2 w-full h-96 border-y-2 border-r-2 rounded-r border-gray-300 outline-none overflow-y-auto font-mono"
           value={text}
           onChange={handleTextAreaChange}
+          onScroll={calculateVisibleLines}
         />
       </div>
     </div>
